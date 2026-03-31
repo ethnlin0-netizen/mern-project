eb6c6b83c3a4123b408bcab020805baa9c9f8027
Ethan Lin <ethnlin0@gmail.com>
2026-03-31 00:33:39 -0400

    added password requirements

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -11,12 +11,12 @@ function Register() {
   function handleSubmit(event: React.MouseEvent<HTMLInputElement>): void {
     event.preventDefault()
     if (password.length < 8) {
-      setMessage('Password must be at least 8 characters')
+      setMessage('Password must be at least 8 characters!')
       return
     }
     
     if (!/[A-Z]/.test(password)) {
-      setMessage('Password must contain at least one uppercase letter')
+      setMessage('Password must contain at least one uppercase letter!')
       return
     }
 
@@ -32,6 +32,7 @@ function Register() {
       <input type="text" placeholder="Login" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setLogin(e.target.value)} /><br />
       <input type="password" placeholder="Password" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} /><br />
       <input type="submit" value="Register" onClick={handleSubmit} />
+      <br />
       <span>{message}</span><br />
       <span>Already have an account? <a href="/login">Login</a></span>
     </div>
